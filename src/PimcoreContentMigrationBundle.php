2b20e9b3b131fdc18e9eff2bb6742acdb28f0c02
Dennis Pansegrau <dennis@pansegrau.biz>
2025-10-15 15:15:36 +0200

    init

---
+++ src/PimcoreContentMigrationBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DennisPansegrau\PimcoreContentMigrationBundle;
+
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+
+final class PimcoreContentMigrationBundle extends AbstractPimcoreBundle
+{
+
+}
