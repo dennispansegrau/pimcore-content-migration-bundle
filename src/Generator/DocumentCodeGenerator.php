6a51bdc7670501e72fda9c530d6d165e3de44b95
Dennis Pansegrau <dennis@pansegrau.biz>
2025-10-28 13:50:41 +0100

    moved builder selection inside the template

--- src/Generator/DocumentCodeGenerator.php
+++ src/Generator/DocumentCodeGenerator.php
@@ -24,24 +24,7 @@ readonly class DocumentCodeGenerator implements CodeGeneratorInterface
 
         return $this->codeGenerator->generate('document_template', [
             'document' => $object,
-            'builder_path' => $this->getBuilderPath($object->getType()),
+            'type' => $object->getType(),
         ]);
-    }
-
-    private function getBuilderPath(string $type): string
-    {
-        return match ($type) {
-            'email' => '\PimcoreContentMigration\Builder\Document\EmailBuilder',
-            'folder' => '\PimcoreContentMigration\Builder\Document\FolderBuilder',
-            'hardlink' => '\PimcoreContentMigration\Builder\Document\HardLinkBuilder',
-            'link' => '\PimcoreContentMigration\Builder\Document\LinkBuilder',
-            'page' => '\PimcoreContentMigration\Builder\Document\PageBuilder',
-            'snippet' => '\PimcoreContentMigration\Builder\Document\SnippetBuilder',
-            'printpage' => '\PimcoreContentMigration\Builder\Document\PrintPageBuilder',
-            'printcontainer' => '\PimcoreContentMigration\Builder\Document\PrintContainerBuilder',
-            'newsletter' => '\PimcoreContentMigration\Builder\Document\NewsletterBuilder',
-            'document' => '\PimcoreContentMigration\Builder\Document\DocumentBuilder',
-            default => throw new \RuntimeException('There is not builder implemented for the document type ' . $type),
-        };
     }
 }
