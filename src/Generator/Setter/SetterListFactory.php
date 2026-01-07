a993c7e075e108ed55e906a5c397ddbeca8226cd
Dennis Pansegrau <dennis@pansegrau.biz>
2026-01-07 13:57:13 +0100

    improved SetterListFactory

--- src/Generator/Setter/SetterListFactory.php
+++ src/Generator/Setter/SetterListFactory.php
@@ -2,49 +2,13 @@
 
 namespace PimcoreContentMigration\Generator\Setter;
 
-use function in_array;
-use function lcfirst;
-
 use Pimcore\Model\DataObject;
-use ReflectionClass;
 use ReflectionException;
-use ReflectionMethod;
 
-use function str_starts_with;
-use function substr;
+use function ucfirst;
 
 class SetterListFactory
 {
-    /** setters to ignore */
-    private const SETTER_FILTERS = [
-        'setId',
-        'setPublished',
-        'setDao',
-        'setDoNotRestoreKeyAndPath',
-        'setDisableDirtyDetection',
-        'setInDumpState',
-        'setParent',
-        'setType',
-        'setKey',
-        'setParentId',
-        'setHideUnpublished',
-        'setVersions',
-        'setVersionCount',
-        'setClass',
-        'setClassName',
-        'setOmitMandatoryCheck',
-        'setGetInheritedValues',
-        'setClassId',
-        'setPath',
-        'setUserModification',
-        'setCreationDate',
-        'setModificationDate',
-        'setUserOwner',
-        'setLocked',
-        'setProperties',
-        'setScheduledTasks',
-    ];
-
     /**
      * @param DataObject $abstractElement
      * @return SetterList
@@ -52,23 +16,20 @@ class SetterListFactory
      */
     public function getList(DataObject $abstractElement): SetterList
     {
-        $reflection = new ReflectionClass($abstractElement::class);
-        $setters = [];
-        foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-            if (str_starts_with($method->getName(), 'set') && $method->getNumberOfParameters() === 1) {
-                if (in_array($method->getName(), self::SETTER_FILTERS, true)) {
-                    continue;
-                }
+        if (!method_exists($abstractElement, 'getClass')) {
+            throw new ReflectionException('DataObject must have a getClass method to generate setter list.');
+        }
+        /** @var DataObject\ClassDefinition $classDefinition */
+        $classDefinition = $abstractElement->getClass();
+        $fieldDefinitions = $classDefinition->getFieldDefinitions();
 
-                $setterName = $method->getName();
-                $getterName = 'g' . substr($setterName, 1);
-                $name = lcfirst(substr($setterName, 3));
-                $setter = new Setter(
-                    $name,
-                    $abstractElement->$getterName()
-                );
-                $setters[$name] = $setter;
-            }
+        $setters = [];
+        foreach ($fieldDefinitions as $fieldName => $fieldDefinition) {
+            $getterName = 'get' . ucfirst($fieldName);
+            $setters[$fieldName] = new Setter(
+                $fieldName,
+                $abstractElement->$getterName()
+            );
         }
         return new SetterList($setters);
     }
