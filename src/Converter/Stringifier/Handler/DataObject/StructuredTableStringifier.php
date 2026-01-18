950928e6b07702e5f318bcc7b2fe1b8e3fe2d7e4
Dennis Pansegrau <dennis@pansegrau.biz>
2026-01-18 13:26:39 +0100

    added StructuredTable handler

---
+++ src/Converter/Stringifier/Handler/DataObject/StructuredTableStringifier.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PimcoreContentMigration\Converter\Stringifier\Handler\DataObject;
+
+use Pimcore\Model\DataObject\Data\StructuredTable;
+use PimcoreContentMigration\Converter\Stringifier\Handler\Trait\ValueToStringConverterTrait;
+use PimcoreContentMigration\Converter\Stringifier\ValueStringifier;
+use PimcoreContentMigration\Generator\Dependency\DependencyList;
+
+class StructuredTableStringifier implements ValueStringifier
+{
+    use ValueToStringConverterTrait;
+
+    public function supports(mixed $value, array $parameters = []): bool
+    {
+        return $value instanceof StructuredTable;
+    }
+
+    public function toString(mixed $value, DependencyList $dependencyList, array $parameters = []): string
+    {
+        /** @var StructuredTable $value */
+        $dataString = $this->getConverter()->convertValueToString($value->getData(), $dependencyList, $parameters);
+        return sprintf('new \Pimcore\Model\DataObject\Data\StructuredTable(%s)', $dataString);
+    }
+}
