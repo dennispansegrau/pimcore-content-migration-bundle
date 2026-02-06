be9d9bbb98644f5bf3f869f4ed5e216f70d0fb37
Dennis Pansegrau <dennis@pansegrau.biz>
2026-02-06 10:48:58 +0100

    added UnitBuilder

--- src/Converter/Stringifier/Handler/DataObject/QuantityValueUnitStringifier.php
+++ src/Converter/Stringifier/Handler/DataObject/QuantityValueUnitStringifier.php
@@ -3,6 +3,7 @@
 namespace PimcoreContentMigration\Converter\Stringifier\Handler\DataObject;
 
 use Pimcore\Model\DataObject\QuantityValue\Unit;
+use PimcoreContentMigration\Builder\DataObject\QuantityValue\UnitBuilder;
 use PimcoreContentMigration\Converter\Stringifier\Handler\Trait\ValueToStringConverterTrait;
 use PimcoreContentMigration\Converter\Stringifier\ValueStringifier;
 use PimcoreContentMigration\Generator\Dependency\DependencyList;
@@ -21,6 +22,34 @@ class QuantityValueUnitStringifier implements ValueStringifier
     public function toString(mixed $value, DependencyList $dependencyList, array $parameters = []): string
     {
         /** @var Unit $value */
-        return sprintf('\Pimcore\Model\DataObject\QuantityValue\Unit::getById(\'%s\')', $value->getId() ?? '');
+        $builderClass = UnitBuilder::class;
+
+        $id = $value->getId();
+        $abbreviation = $value->getAbbreviation();
+        $longName = $value->getLongName();
+        $baseUnit = $value->getBaseunit();
+        $factor = $value->getFactor();
+        $converterOffset = $value->getConversionOffset();
+        $converter = $value->getConverter();
+
+        $idString = $this->getConverter()->convertValueToString($id, $dependencyList, $parameters);
+        $abbreviationString = $this->getConverter()->convertValueToString($abbreviation, $dependencyList, $parameters);
+        $longNameString = $this->getConverter()->convertValueToString($longName, $dependencyList, $parameters);
+        $baseUnitString = $this->getConverter()->convertValueToString($baseUnit, $dependencyList, $parameters);
+        $factorString = $this->getConverter()->convertValueToString($factor, $dependencyList, $parameters);
+        $converterOffsetString = $this->getConverter()->convertValueToString($converterOffset, $dependencyList, $parameters);
+        $converterString = $this->getConverter()->convertValueToString($converter, $dependencyList, $parameters);
+
+        return sprintf(
+            '\%s::findOrCreate(%s, %s, %s, %s, %s, %s, %s)',
+            $builderClass,
+            $idString,
+            $abbreviationString,
+            $longNameString,
+            $baseUnitString,
+            $factorString,
+            $converterOffsetString,
+            $converterString
+        );
     }
 }
