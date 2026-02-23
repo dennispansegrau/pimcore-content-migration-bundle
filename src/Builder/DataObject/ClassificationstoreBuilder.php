9d5453a85d169fe286083939dbae8d53b696fed2
Dennis Pansegrau <dennis@pansegrau.biz>
2026-02-23 15:21:37 +0100

    Classificationstore Builder and Stringifier

--- src/Builder/DataObject/ClassificationstoreBuilder.php
+++ src/Builder/DataObject/ClassificationstoreBuilder.php
@@ -2,44 +2,290 @@
 
 namespace PimcoreContentMigration\Builder\DataObject;
 
+use Exception;
+
+use function is_string;
+
 use LogicException;
+
+use function method_exists;
+
 use Pimcore\Model\DataObject\Classificationstore;
+use Pimcore\Model\DataObject\Classificationstore\CollectionConfig;
+use Pimcore\Model\DataObject\Classificationstore\CollectionGroupRelation;
+use Pimcore\Model\DataObject\Classificationstore\GroupConfig;
+use Pimcore\Model\DataObject\Classificationstore\KeyConfig;
+use Pimcore\Model\DataObject\Classificationstore\KeyGroupRelation;
+use Pimcore\Model\DataObject\Classificationstore\StoreConfig;
 use Pimcore\Model\DataObject\Concrete;
 
 class ClassificationstoreBuilder
 {
     private ?Classificationstore $classificationstore = null;
 
+    private ?int $storeId = null;
+
+    /** @var array<string, int> */
+    private array $keysNameIdMapping = [];
+
+    /** @var array<string, int> */
+    private array $groupNameIdMapping = [];
+
+    /** @var array<string, int> */
+    private array $collectionNameIdMapping = [];
+
     final protected function __construct()
     {
     }
 
-    /**
-     * @param string $fieldName
-     * @param Concrete $owner
-     * @param array<int, array<int, array<string, mixed>>> $items
-     * @return static
-     */
-    public static function create(string $fieldName, Concrete $owner, array $items): static
+    public static function createOrUpdate(string $fieldName, Concrete $owner, string $storeName, string $storeDescription): static
     {
         $builder = new static();
-        $builder->classificationstore = new Classificationstore();
-        $builder->classificationstore->setFieldname($fieldName);
-        $builder->classificationstore->setObject($owner);
 
-        $activeGroups = [];
+        $storeConfig = self::createOrUpdateStoreConfig($storeName, $storeDescription);
+        $builder->storeId = $storeConfig->getId();
+        if ($builder->storeId === null) {
+            throw new LogicException('Store Config id has not been set');
+        }
+
+        $classificationStore = $owner->get($fieldName);
+        if (!$classificationStore instanceof Classificationstore) {
+            throw new LogicException("Classificationstore $fieldName not found in object");
+        }
+        $builder->classificationstore = $classificationStore;
+        return $builder;
+    }
+
+    /**
+     * @param array<array<string, bool|string>> $keys
+     * @return $this
+     */
+    public function updateKeys(array $keys): static
+    {
+        foreach ($keys as $keyData) {
+            if (!isset($keyData['name']) || !is_string($keyData['name'])) {
+                throw new LogicException('Invalid key name');
+            }
+
+            $key = KeyConfig::getByName($keyData['name'], $this->getStoreId());
+            if ($key === null) {
+                $key = new KeyConfig();
+                $key->setStoreId($this->getStoreId());
+                $key->setName($keyData['name']);
+            }
+
+            if (isset($keyData['title']) && is_string($keyData['title'])) {
+                $key->setTitle($keyData['title']);
+            }
+
+            if (isset($keyData['description']) && is_string($keyData['description'])) {
+                $key->setDescription($keyData['description']);
+            }
+
+            if (isset($keyData['enabled'])) {
+                $key->setEnabled((bool)($keyData['enabled']));
+            }
+
+            if (isset($keyData['type']) && is_string($keyData['type'])) {
+                $key->setType($keyData['type']);
+            }
+
+            if (isset($keyData['definition']) && is_string($keyData['definition'])) {
+                $key->setDefinition($keyData['definition']); // JSON string
+            }
+
+            $key->save();
+
+            $this->keysNameIdMapping[$key->getName()] = (int) $key->getId();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<array<string, string>> $groups
+     * @return $this
+     */
+    public function updateGroups(array $groups): static
+    {
+        foreach ($groups as $groupData) {
+            if (!isset($groupData['name']) || !is_string($groupData['name'])) {
+                throw new LogicException('Invalid group name');
+            }
+
+            $group = GroupConfig::getByName($groupData['name'], $this->getStoreId());
+            if ($group === null) {
+                $group = new GroupConfig();
+                $group->setStoreId($this->getStoreId());
+                $group->setName($groupData['name']);
+            }
+
+            if (isset($groupData['description'])) {
+                $group->setDescription($groupData['description']);
+            }
+
+            $group->save();
+
+            $this->groupNameIdMapping[$group->getName()] = (int) $group->getId();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<array<string, string>> $collections
+     * @return $this
+     */
+    public function updateCollections(array $collections): static
+    {
+        foreach ($collections as $collectionData) {
+            if (!isset($collectionData['name']) || !is_string($collectionData['name'])) {
+                throw new LogicException('Invalid collection name');
+            }
+
+            $collection = CollectionConfig::getByName($collectionData['name'], $this->getStoreId());
+            if ($collection === null) {
+                $collection = new CollectionConfig();
+                $collection->setStoreId($this->getStoreId());
+                $collection->setName($collectionData['name']);
+            }
+
+            if (isset($collectionData['description'])) {
+                $collection->setDescription($collectionData['description']);
+            }
+
+            $collection->save();
+
+            $this->collectionNameIdMapping[$collection->getName()] = (int) $collection->getId();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<array<string, string|int|bool>> $relations
+     * @return $this
+     */
+    public function updateKeyGroupRelations(array $relations): static
+    {
+        foreach ($relations as $relationData) {
+            if (!isset($relationData['group_name']) && !is_string($relationData['group_name'])) {
+                throw new LogicException('Invalid group_name');
+            }
+
+            if (!isset($relationData['key_name']) && !is_string($relationData['key_name'])) {
+                throw new LogicException('Invalid key_name');
+            }
+
+            $groupId = $this->groupNameIdMapping[$relationData['group_name']] ?? null;
+            $keyId = $this->keysNameIdMapping[$relationData['key_name']] ?? null;
+            if (!$groupId || !$keyId) {
+                throw new LogicException('Could not create group-key relation');
+            }
+
+            $relation = KeyGroupRelation::getByGroupAndKeyId($groupId, $keyId);
+            if ($relation === null) {
+                $relation = new KeyGroupRelation();
+                $relation->setGroupId($groupId);
+                $relation->setKeyId($keyId);
+            }
+
+            // depends on pimcore version
+            // @phpstan-ignore-next-line function.alreadyNarrowedType
+            if (method_exists($relation, 'setSorter') && isset($relationData['sorter'])) {
+                $relation->setSorter((int)$relationData['sorter']);
+            }
+
+            // depends on pimcore version
+            // @phpstan-ignore-next-line function.alreadyNarrowedType
+            if (method_exists($relation, 'setMandatory') && isset($relationData['mandatory'])) {
+                $relation->setMandatory((bool)$relationData['mandatory']);
+            }
+
+            $relation->save();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<array<string, string|int>> $relations
+     * @return $this
+     */
+    public function updateGroupCollectionRelations(array $relations): static
+    {
+        foreach ($relations as $relationData) {
+            if (!isset($relationData['group_name']) && !is_string($relationData['group_name'])) {
+                throw new LogicException('Invalid group_name');
+            }
+
+            if (!isset($relationData['collection_name']) && !is_string($relationData['collection_name'])) {
+                throw new LogicException('Invalid collection_name');
+            }
+
+            $groupId = $this->groupNameIdMapping[$relationData['group_name']] ?? null;
+            $collectionId = $this->collectionNameIdMapping[$relationData['collection_name']] ?? null;
+            if (!$groupId || !$collectionId) {
+                throw new LogicException('Could not create collection-group relation');
+            }
+
+            $relation = CollectionGroupRelation::getByGroupAndColId($groupId, $collectionId);
+            if ($relation === null) {
+                $relation = new CollectionGroupRelation();
+                $relation->setGroupId($groupId);
+                $relation->setColId($collectionId);
+            }
+
+            // depends on pimcore version
+            // @phpstan-ignore-next-line function.alreadyNarrowedType
+            if (method_exists($relation, 'setSorter') && isset($relationData['sorter'])) {
+                $relation->setSorter((int)$relationData['sorter']);
+            }
+
+            $relation->save();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array<int, array<int, array<string, mixed>>> $items
+     * @return $this
+     * @throws Exception
+     */
+    public function setItems(array $items): static
+    {
         foreach ($items as $groupId => $keys) {
-            $activeGroups[$groupId] = true;
-
             foreach ($keys as $keyId => $languages) {
                 foreach ($languages as $language => $value) {
-                    $builder->classificationstore->setLocalizedKeyValue($groupId, $keyId, $value, $language);
+                    $this->getObject()->setLocalizedKeyValue($groupId, $keyId, $value, $language);
                 }
             }
         }
+        $this->getObject()->save();
+        return $this;
+    }
 
-        $builder->classificationstore->setActiveGroups($activeGroups);
-        return $builder;
+    /**
+     * @param array<int, bool> $activeGroups
+     * @return $this
+     */
+    public function setActiveGroups(array $activeGroups): static
+    {
+        $this->getObject()->setActiveGroups($activeGroups);
+        $this->getObject()->save();
+        return $this;
+    }
+
+    /**
+     * @param array<int, int> $groupCollectionMapping
+     * @return $this
+     */
+    public function setGroupCollectionMapping(array $groupCollectionMapping): static
+    {
+        $this->getObject()->setGroupCollectionMappings($groupCollectionMapping);
+        $this->getObject()->save();
+        return $this;
     }
 
     public function getObject(): Classificationstore
@@ -48,5 +294,30 @@ class ClassificationstoreBuilder
             throw new LogicException('Classificationstore object has not been set');
         }
         return $this->classificationstore;
+    }
+
+    /**
+     * @param string $storeName
+     * @param string $storeDescription
+     * @return StoreConfig
+     */
+    private static function createOrUpdateStoreConfig(string $storeName, string $storeDescription): StoreConfig
+    {
+        $storeConfig = StoreConfig::getByName($storeName);
+        if (!$storeConfig instanceof StoreConfig) {
+            $storeConfig = new StoreConfig();
+            $storeConfig->setName($storeName);
+        }
+        $storeConfig->setDescription($storeDescription);
+        $storeConfig->save();
+        return $storeConfig;
+    }
+
+    private function getStoreId(): int
+    {
+        if ($this->storeId === null) {
+            throw new LogicException('Classificationstore object has not been set');
+        }
+        return $this->storeId;
     }
 }
--- src/Converter/Stringifier/Handler/DataObject/ClassificationstoreStringifier.php
+++ src/Converter/Stringifier/Handler/DataObject/ClassificationstoreStringifier.php
@@ -3,19 +3,40 @@
 namespace PimcoreContentMigration\Converter\Stringifier\Handler\DataObject;
 
 use function array_key_exists;
+use function array_map;
+use function in_array;
 use function is_string;
 
+use LogicException;
 use Pimcore\Model\DataObject\Classificationstore;
+use Pimcore\Model\DataObject\Classificationstore\CollectionConfig;
+use Pimcore\Model\DataObject\Classificationstore\CollectionGroupRelation;
+use Pimcore\Model\DataObject\Classificationstore\GroupConfig;
+use Pimcore\Model\DataObject\Classificationstore\KeyConfig;
+use Pimcore\Model\DataObject\Classificationstore\KeyGroupRelation;
+use Pimcore\Model\DataObject\Classificationstore\StoreConfig;
 use PimcoreContentMigration\Builder\DataObject\ClassificationstoreBuilder;
+use PimcoreContentMigration\Converter\Stringifier\Handler\Trait\IndentTrait;
 use PimcoreContentMigration\Converter\Stringifier\Handler\Trait\ValueToStringConverterTrait;
 use PimcoreContentMigration\Converter\Stringifier\ValueStringifier;
 use PimcoreContentMigration\Generator\Dependency\DependencyList;
 
 use function sprintf;
+use function str_repeat;
 
 class ClassificationstoreStringifier implements ValueStringifier
 {
     use ValueToStringConverterTrait;
+    use IndentTrait;
+
+    /** @var array<int, string> */
+    private array $keysIdNameMapping = [];
+
+    /** @var array<int, string> */
+    private array $groupIdNameMapping = [];
+
+    /** @var array<int, string> */
+    private array $collectionIdNameMapping = [];
 
     public function supports(mixed $value, array $parameters = []): bool
     {
@@ -26,12 +47,247 @@ class ClassificationstoreStringifier implements ValueStringifier
     {
         /** @var Classificationstore $value */
         $builderName = ClassificationstoreBuilder::class;
+        $owner = $this->getOwner($parameters);
+
+        $storeId = $this->getStoreId($value);
+        $storeConfig = $this->getStoreConfig($storeId);
+
+        $spacing = str_repeat(' ', $this->getAndIncreaseIndent($parameters) + 4);
+
+        // structure data
+        $keysString = $this->getConverter()->convertValueToString($this->getKeys($storeId), $dependencyList, $parameters);
+        $groupsString = $this->getConverter()->convertValueToString($this->getGroups($storeId), $dependencyList, $parameters);
+        $collectionsString = $this->getConverter()->convertValueToString($this->getCollections($storeId), $dependencyList, $parameters);
+        $keyGroupRelationsString = $this->getConverter()->convertValueToString($this->getKeyGroupRelations(), $dependencyList, $parameters);
+        $groupCollectionRelationString = $this->getConverter()->convertValueToString($this->getGroupCollectionRelation(), $dependencyList, $parameters);
+
+        // actual classification store data
+        $itemsString = $this->getConverter()->convertValueToString($value->getItems(), $dependencyList, $parameters);
+        $activeGroupString = $this->getConverter()->convertValueToString($this->getActiveGroups($value), $dependencyList, $parameters);
+        $groupCollectionMappingString = $this->getConverter()->convertValueToString($this->getGroupCollectionMapping($value), $dependencyList, $parameters);
+
+        return sprintf(
+            "\\%s::createOrUpdate('%s', %s, '%s', '%s')\n" .
+                "%s->updateKeys(%s)\n" .
+                "%s->updateGroups(%s)\n" .
+                "%s->updateCollections(%s)\n" .
+                "%s->updateKeyGroupRelations(%s)\n" .
+                "%s->updateGroupCollectionRelations(%s)\n" .
+                "%s->setItems(%s)\n" .
+                "%s->setActiveGroups(%s)\n" .
+                "%s->setGroupCollectionMapping(%s)\n" .
+                '%s->getObject()',
+            $builderName,
+            $value->getFieldname(),
+            $owner,
+            $storeConfig->getName() ?? '',
+            $storeConfig->getDescription() ?? '',
+            $spacing,
+            $keysString,
+            $spacing,
+            $groupsString,
+            $spacing,
+            $collectionsString,
+            $spacing,
+            $keyGroupRelationsString,
+            $spacing,
+            $groupCollectionRelationString,
+            $spacing,
+            $itemsString,
+            $spacing,
+            $activeGroupString,
+            $spacing,
+            $groupCollectionMappingString,
+            $spacing,
+        );
+    }
+
+    /**
+     * @param int $storeId
+     * @return array<array<string, bool|string>>
+     */
+    public function getKeys(int $storeId): array
+    {
+        $keys = (new KeyConfig\Listing())->setCondition('storeId = ?', [$storeId])->load();
+
+        foreach ($keys as $key) {
+            if (in_array($key->getName(), $this->keysIdNameMapping, true)) {
+                throw new LogicException(sprintf('The Key "%s" in the Classification Store %d is not unique. No mapping possible.', $key->getName(), $storeId));
+            }
+            $this->keysIdNameMapping[(int) $key->getId()] = $key->getName();
+        }
+
+        return array_map(fn ($key) => [
+            'name' => $key->getName(),
+            'title' => $key->getTitle() ?? '',
+            'description' => $key->getDescription() ?? '',
+            'enabled' => $key->getEnabled(),
+            'type' => $key->getType(),
+            'definition' => $key->getDefinition(), // JSON string (FieldDefinition)
+        ], $keys);
+    }
+
+    /**
+     * @param int $storeId
+     * @return array<array<string, string>>
+     */
+    public function getGroups(int $storeId): array
+    {
+        $groups = (new GroupConfig\Listing())->setCondition('storeId = ?', [$storeId])->load();
+
+        foreach ($groups as $group) {
+            if (in_array($group->getName(), $this->groupIdNameMapping, true)) {
+                throw new LogicException(sprintf('The Group "%s" in the Classification Store %d is not unique. No mapping possible.', $group->getName(), $storeId));
+            }
+            $this->groupIdNameMapping[(int) $group->getId()] = $group->getName();
+        }
+
+        return array_map(fn ($group) => [
+            'name' => $group->getName(),
+            'description' => $group->getDescription() ?? '',
+        ], $groups);
+    }
+
+    /**
+     * @param int $storeId
+     * @return array<array<string, string>>
+     */
+    public function getCollections(int $storeId): array
+    {
+        $collectionConfigs = (new CollectionConfig\Listing())->setCondition('storeId = ?', [$storeId])->load();
+
+        foreach ($collectionConfigs as $collectionConfig) {
+            if (in_array($collectionConfig->getName(), $this->collectionIdNameMapping, true)) {
+                throw new LogicException(sprintf('The Group Collection "%s" in the Classification Store %d is not unique. No mapping possible.', $collectionConfig->getName(), $storeId));
+            }
+            $this->collectionIdNameMapping[(int) $collectionConfig->getId()] = $collectionConfig->getName();
+        }
+
+        return array_map(fn ($collectionConfig) => [
+            'name' => $collectionConfig->getName(),
+            'description' => $collectionConfig->getDescription(),
+        ], $collectionConfigs);
+    }
+
+    /**
+     * @return array<array<string, string|int|bool>>
+     */
+    public function getKeyGroupRelations(): array
+    {
+        $keyGroupRels = (new KeyGroupRelation\Listing())->load();
+        $keyGroupRelations = [];
+
+        // Key ↔ Group
+        foreach ($keyGroupRels as $relation) {
+            if (!isset($this->groupIdNameMapping[$relation->getGroupId()]) ||
+                !isset($this->keysIdNameMapping[$relation->getKeyId()])) {
+                continue; // does not belong to store id
+            }
+
+            $groupName = $this->groupIdNameMapping[$relation->getGroupId()];
+            $keyName = $this->keysIdNameMapping[$relation->getKeyId()];
+
+            $keyGroupRelations[] = [
+                'group_name' => $groupName,
+                'key_name' => $keyName,
+                'sorter' => $relation->getSorter(),
+                'mandatory' => $relation->isMandatory(),
+            ];
+        }
+
+        return $keyGroupRelations;
+    }
+
+    /**
+     * @return array<array<string, string|int>>
+     */
+    public function getGroupCollectionRelation(): array
+    {
+        $colGroupRels = (new CollectionGroupRelation\Listing())->load();
+        $collectionGroupRelations = [];
+
+        foreach ($colGroupRels as $relation) {
+            if (!isset($this->groupIdNameMapping[$relation->getGroupId()]) ||
+                !isset($this->collectionIdNameMapping[$relation->getColId()])) {
+                continue; // does not belong to store id
+            }
+
+            $collectionName = $this->collectionIdNameMapping[$relation->getColId()];
+            $groupName = $this->groupIdNameMapping[$relation->getGroupId()];
+
+            $collectionGroupRelations[] = [
+                'collection_name' => $collectionName,
+                'group_name' => $groupName,
+                'sorter' => $relation->getSorter(),
+            ];
+        }
+
+        return $collectionGroupRelations;
+    }
+
+    /**
+     * @param Classificationstore $classificationStore
+     * @return array<int, int>
+     */
+    public function getGroupCollectionMapping(Classificationstore $classificationStore): array
+    {
+        $groupCollectionMapping = [];
+        foreach ($classificationStore->getActiveGroups() as $groupId => $value) {
+            $collectionId = $classificationStore->getGroupCollectionMapping((int) $groupId);
+            if ($collectionId !== null) {
+                $groupCollectionMapping[$groupId] = $collectionId;
+            }
+        }
+        return $groupCollectionMapping;
+    }
+
+    /**
+     * @param array<string, mixed> $parameters
+     * @return string
+     */
+    private function getOwner(array $parameters): string
+    {
         $owner = '$builder->getObject()';
         if (array_key_exists('owner', $parameters) &&
             is_string($parameters['owner'])) {
             $owner = $parameters['owner'];
         }
-        $items = $this->getConverter()->convertValueToString($value->getItems(), $dependencyList, $parameters);
-        return sprintf('\%s::create(\'%s\', %s, %s)->getObject()', $builderName, $value->getFieldname(), $owner, $items);
+        return $owner;
+    }
+
+    /**
+     * @param Classificationstore $classificationStore
+     * @return array<int, bool>
+     */
+    public function getActiveGroups(Classificationstore $classificationStore): array
+    {
+        return $classificationStore->getActiveGroups();
+    }
+
+    /**
+     * @param Classificationstore $classificationStore
+     * @return int
+     */
+    private function getStoreId(Classificationstore $classificationStore): int
+    {
+        $classDefinition = $classificationStore->getClass();
+        $fieldDefinition = $classDefinition?->getFieldDefinition($classificationStore->getFieldname());
+        if (!$fieldDefinition instanceof \Pimcore\Model\DataObject\ClassDefinition\Data\Classificationstore) {
+            throw new LogicException('FieldDefinition expected.');
+        }
+        return $fieldDefinition->getStoreId();
+    }
+
+    /**
+     * @param int $storeId
+     * @return StoreConfig
+     */
+    private function getStoreConfig(int $storeId): StoreConfig
+    {
+        $storeConfig = StoreConfig::getById($storeId);
+        if (!$storeConfig instanceof StoreConfig) {
+            throw new LogicException(sprintf('StoreConfig with id %d not found', $storeId));
+        }
+        return $storeConfig;
     }
 }
