1e4cb733816e8b20a5c4e13de63c12325daf0f8c
Dennis Pansegrau <dennis@pansegrau.biz>
2026-03-12 16:56:28 +0100

    phpstan fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 require \dirname(__DIR__) . '/vendor/autoload.php';
-require __DIR__ . '/stubs/pimcore-stubs.php';
+require __DIR__ . '/stubs/pimcore-stubs.inc';
 require __DIR__ . '/Support/UnknownElement.php';
